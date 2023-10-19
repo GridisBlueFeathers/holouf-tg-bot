71bb63f414599d80869802f7422a63a7b8d05b2d
Serhii Veretennikov <gridis72@gmail.com>
2023-10-19 12:02:47 +0200

    chore: check if webhook works

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,8 +1,15 @@
-export async function GET(request: Request) {
-    console.log("yo")
-    return new Response("hi")
+export async function POST(request: Request) {
+    const req = await request.json()
+    await fetch(`https://api.telegram.org/bot${process.env.TG_BOT_KEY}/sendMessage`, {
+        method: "POST",
+        headers: {
+        "Content-type": "application/json"
+        },
+        body: JSON.stringify({
+            chat_id: process.env.MY_TG_ID,
+            text: JSON.stringify(req) 
+        }) 
+    })
+
+    return new Response(JSON.stringify({hey: "HEY"}))
 }
-
-//export async function POST(request: Request) {
-
-//}
