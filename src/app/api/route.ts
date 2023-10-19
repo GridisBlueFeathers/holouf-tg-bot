131c3326467a2983a37378475f74a1d0ae3eaca5
Serhii Veretennikov <gridis72@gmail.com>
2023-10-20 01:13:59 +0200

    fix: fixed returning from handlers

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -11,19 +11,21 @@ export async function POST(request: Request) {
     // this handles bot commands
     if (update.message.entities && update.message.entities.filter(entity => entity.type === "bot_command").length) {
         const command = update.message.entities.filter(entity => entity.type === "bot_command")[0];
-        const commandName = update.message.text.slice(command.offset + 1, command.offset + command.length)
+        const commandName = update.message.text.slice(command.offset + 1, command.offset + command.length);
 
         switch (commandName) {
             case "yo":
-                sendMessage({message: "yo yo", chat: update.message.chat})
+                sendMessage({message: "yo yo", chat: update.message.chat});
                 break;
-        }
-    }
+        };
+        return new Response("OK");
+    };
 
     // this handles other messages in private chats
     if (update.message.chat.type === "private") {
-        sendMessage({message: update.message.text, chat: update.message.chat})
-    }
+        sendMessage({message: update.message.text, chat: update.message.chat});
+        return new Response("OK");
+    };
 
-    return new Response("OK")
-}
+    return new Response("OK");
+};
